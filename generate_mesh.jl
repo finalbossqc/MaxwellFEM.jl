85064af535d492c2cc19edf99731c011c6a79e1a
finalbossqc <mastercoder7200@gmail.com>
2025-06-18 14:45:46 -0500

    Added 2D mesh generation

--- generate_mesh.jl
+++ generate_mesh.jl
@@ -1,14 +1,16 @@
-function generate_random_points(N::Int64, Lx::Float64, Ly::Float64; rng=Random.default_rng())
+function generate_random_points_2D(N::Int64, Lx::Float64, Ly::Float64; rng=Random.default_rng())
     xs = rand(rng, N) .* Lx
     ys = rand(rng, N) .* Ly
 
-    points = [Point2(x, y) for (x, y) in zip(xs, ys)]
+    println(xs)
 
-    return points
+    points = Meshes.Point.(xs, ys)
+    
+    return PointSet(points)
 end
 
-function generate_random_mesh_2D(N::Int, Lx::Float64, Ly::Float64, material::Material; method=DelaunayTesselation(), seed=Random.default_rng())
-    points = generate_random_points(N, Lx, Ly; seed=seed)
+function generate_random_mesh_2D(N::Int, Lx::Float64, Ly::Float64, material::Material; method=DelaunayTesselation(), rng=Random.default_rng())
+    points = generate_random_points_2D(N, Lx, Ly; rng=rng)
     mesh = tesselate(points, method)
     return mesh
 end
