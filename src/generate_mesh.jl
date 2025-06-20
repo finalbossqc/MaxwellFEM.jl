deb8070d25504d75c6e0a8e0691d6498107795f1
finalbossqc <mastercoder7200@gmail.com>
2025-06-20 09:52:08 -0500

    Created src directory

---
+++ src/generate_mesh.jl
@@ -0,0 +1,12 @@
+function generate_random_points_2D(N::Int64, Lx::Float64, Ly::Float64; rng=Random.default_rng())
+    xs = rand(rng, N) .* Lx
+    ys = rand(rng, N) .* Ly
+    points = Meshes.Point.(xs, ys)
+    return PointSet(points)
+end
+
+function generate_random_mesh_2D(N::Int, Lx::Float64, Ly::Float64, material::Material; method=DelaunayTesselation(), rng=Random.default_rng())
+    points = generate_random_points_2D(N, Lx, Ly; rng=rng)
+    mesh = tesselate(points, method)
+    return mesh
+end
