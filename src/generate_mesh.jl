0dcb16fc91665d6a8386ba1c2058f57371acdd49
finalbossqc <mastercoder7200@gmail.com>
2025-06-23 07:55:01 -0500

    Fixed small error in generate_mesh.jl

--- src/generate_mesh.jl
+++ src/generate_mesh.jl
@@ -7,9 +7,9 @@ end
 
 function sample_boundary_2D(N::Int64, Lx::Float64, Ly::Float64; rng=Random.default_rng())
     boundary_points = [Meshes.Point(point.coords.x.val, 0) for point in sample_domain_1D(Int(sqrt(N)), Lx)]
-    boundary_points = boundary_points ∪ [Meshes.Point(point.coords.x.val, Ly) for point in sample_domain_1D(Int(sqrt(N)), Lx)]
-    boundary_points = boundary_points ∪ [Meshes.Point(0, point.coords.x.val) for point in sample_domain_1D(Int(sqrt(N)), Lx)]
-    boundary_points = boundary_points ∪ [Meshes.Point(Lx, point.coords.x.val) for point in sample_domain_1D(Int(sqrt(N)), Lx)]
+    append!(boundary_points, [Meshes.Point(point.coords.x.val, Ly) for point in sample_domain_1D(Int(sqrt(N)), Lx)])
+    append!(boundary_points, [Meshes.Point(0, point.coords.x.val) for point in sample_domain_1D(Int(sqrt(N)), Ly)])
+    append!(boundary_points, [Meshes.Point(Lx, point.coords.x.val) for point in sample_domain_1D(Int(sqrt(N)), Ly)])
     return Vector{Meshes.Point}(boundary_points)
 end
 
